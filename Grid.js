f604fa6f399f531c5fb14738b9f70a2f9178af31
Mger-arzi <mger54598@gmail.com>
2023-07-31 09:53:14 +0300

    3

--- Grid.js
+++ Grid.js
@@ -1,3 +1,5 @@
+import{ Cell } from "./Cell.js"
+
 const GRID_SIZE = 4;
 const CELLS_COUNT = GRID_SIZE * GRID_SIZE
 
@@ -5,5 +7,10 @@ const CELLS_COUNT = GRID_SIZE * GRID_SIZE
 export class Grid {
 	constructor(gridElement){
 		this.cells = []
+		for(let i = 0; i < CELLS_COUNT; i++){
+			this.cells.push(
+				new Cell(gridElement, x , y)
+			)
+		}
 	}
 }
